98ccc05a16f71f286824733879ece1f4cff258c6
Rais <raishab@yandex.ru>
2018-06-19 00:21:59 +0400

    added support of unlimited amount of variables in a link

--- website.py
+++ website.py
@@ -1,4 +1,14 @@
-def picture_row(page_name, page_num):
+def picture_row(page_name, page_num, section, n):
+    way = str(page_name) + '/'
+    if section != None:
+      section = [i for i in section.split()]
+      for k in range(n):
+  	 try:     
+	      section_name = eval('section[k]')
+	      section_name += '/'
+              way += section_name
+         except:
+              pass
     pic_code = dict()
     pic_is = True
     image_counter = 0
@@ -12,18 +22,18 @@ def picture_row(page_name, page_num):
             page_counter += 1
             page_image_counter = 1
             pic_code[str(page_counter)] = '<div class="row">'
-        pic_code[str(page_counter)] = pic_code[str(page_counter)] + ' ' + '<a href="/static/images/'+str(page_name)+'/' + str(image_counter) + '.jpg" data-fancybox="images" class="col-md-4 col-xs-4 col-sm-4"><img src="/static/images/'+str(page_name)+'/' + str(image_counter) + '.jpg" class="img-thumbnail"></a>'
+        pic_code[str(page_counter)] = pic_code[str(page_counter)] + ' ' + '<a href="' + '/static/images/' + way + str(image_counter) + '.jpg" data-fancybox="images" class="col-md-4 col-xs-4 col-sm-4"><img src="/static/images/'+ way + str(image_counter) + '.jpg" class="img-thumbnail"></a>'
 
         try:
-            open('static/images/'+str(page_name)+'/' + str(image_counter + 1) + '.jpg')
+            open('static/images/' + way + str(image_counter + 1) + '.jpg')
         except:
             pic_is = False
     pic_code['num'] = '<div class="btn-group col-xs-12">'
     for k in range(page_counter):
         if k == int(page_num) - 1:
-            pic_code['num'] = pic_code['num'] + '<a href="/'+page_name+'/'+str(k+1)+'" class="btn active">'+str(k+1)+'</a>'
+            pic_code['num'] = pic_code['num'] + '<a href="/' + way +str(k+1)+'" class="btn active">'+str(k+1)+'</a>'
         else:
-            pic_code['num'] = pic_code['num'] + '<a href="/'+page_name+'/'+str(k+1)+'" class="btn">'+str(k+1)+'</a>'
+            pic_code['num'] = pic_code['num'] + '<a href="/' + way +str(k+1)+'" class="btn">'+str(k+1)+'</a>'
         if k == page_counter - 1:
             pic_code['num'] = pic_code['num'] + '</div></div>'
     pic_code = pic_code[page_num] + pic_code['num']

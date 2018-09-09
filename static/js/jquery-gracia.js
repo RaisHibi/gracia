b226f7e893d080574cf6541af432306569bbabef
Rais <raishab@yandex.ru>
2018-09-09 20:30:58 +0400

    added calculator

--- static/js/jquery-gracia.js
+++ static/js/jquery-gracia.js
@@ -91,4 +91,99 @@ $(document).ready(function() {
 	});
 }); 
 
+(function ($) {
+  $(function () {
+ 
+        calculate();
+ 
+        jQuery('#calculator input').keyup(function() {
+            this.value = this.value.replace(/[^0-9\.,]/g, '');
+            this.value = this.value.replace(/[,]/, '.');
+        });
+        jQuery('#calculator input, #calculator select').change(function() {
+            calculate();
+        });
+        jQuery('#calculator input').keyup(function() {
+            calculate();
+        });
+        function calculate() {
+      $('.calculator').each(function(key, val){
+        calcInputs = {};
+            $(this).find('input, select').each(function(key, val){
+                name = $(this).attr('name');
+                val = $(this).val();
+                if (!$.isNumeric(val)) {
+                    switch (name) {
+                        case 'area':
+                            val = 1;
+                            break;
+                        case 'corners':
+                            val = 4;
+                            break;
+                        case 'lamp':
+                            val = '';
+                            break;
+                        case 'tube':
+                            val = '';
+                            break;
+                        case 'chandelier-hook':
+                            val = '';
+                            break;
+                        default:
+                            break;
+                    }
+                    $(this).val(val);
+                }
+                calcInputs[name] = val;
+            });
+ 
+            total = 0;
+ 
+            switch (calcInputs.texture) {
+                case 'mat':
+            total += calcInputs.area * 350;
+            break;
+                case 'glossy':
+            total += calcInputs.area * 350;
+            break;
+                case 'satin':
+            total += calcInputs.area * 350;
+            break;
+                case 'fabric':
+            total += calcInputs.area * 400;
+            break;
+            default:
+            break;
+            }
+ 
+            // каждый угол
+                total += calcInputs.corners * 40;
+ 
+            // установка люстры
+            if (calcInputs['chandelier-hook'] >= 1) {
+                total += calcInputs['chandelier-hook'] * 300;
+            }
+            // установка встраемого светильника
+            if (calcInputs.lamp >= 1) {
+                total += calcInputs.lamp * 300;
+            }
+            // обход трубы
+            if (calcInputs.tube >= 1) {
+                total += calcInputs.tube * 200;
+            }
+ 
+        install = calcInputs.area * 0;
+        total += install;
+	    if (total < 4000) {
+	        total = 4000
+	    }
+ 
+            total += ' руб.';
+            jQuery(this).find('span.total').html(total);
+      });
+        }
+ 
+    });
+})(jQuery);
+;
 

4de5df523fa0beebd99b4d4f73b68a604dc7d589
Ubuntu <ubuntu@ip-172-31-37-208.us-east-2.compute.internal>
2019-07-15 13:17:08 +0000

    update

--- main.py
+++ main.py
@@ -56,4 +56,4 @@ def korp_meb():
 def myagk_meb():
     return render_template('myagk_meb.html')
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000, debug=True)
